263d2011d022b41cdafd92729416d606b53c2929
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-03-02 14:37:01 -0300

    WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
     extends: [
         '@cknow/eslint-config/esnext',
-        '@cknow/eslint-config/plugins/mocha'
+        '@cknow/eslint-config/plugins/mocha',
+        '@cknow/eslint-config/plugins/node'
     ],
     rules: {
-        'mocha/no-mocha-arrows': 'off'
+        'mocha/no-mocha-arrows': 'off',
+
+        'node/no-extraneous-import': 'off',
+        'node/no-extraneous-require': 'off',
+        'node/no-unsupported-features': 'off'
     }
 };
